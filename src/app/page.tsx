3950f8ddf71bbad5d3c43f8b6e07158c36419703
Dusan <dushan.dule@gmail.com>
2025-10-19 20:21:32 +0200

    Created the login page. Looks similar to prototype

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,136 @@
-import Image from "next/image";
+'use client'
+
+import { useState } from "react";
 import styles from "./page.module.css";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import Swal from "sweetalert2";
 
-export default function Home() {
+export default function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  let users = [
+    { username: "dusan", password: "delecar123"},
+    { username: "rados", password: "bajic321" }
+  ]
+
+  let userData = [
+    { username: "dusan", password: "delecar123", name: "Dusan", surname: "Milovanovic", address: "Oslobodjenja 7", phone: "063 17 300 22" },
+    { username: "rados", password: "bajic321", name: "Rados", surname: "Bajic", address: "Kukljin bb", phone: "067 804 557" }
+  ]
+
+  const router = useRouter();
+
+  function loginUser(e) {
+    e.preventDefault();
+
+    // ako sam upamtila tog usera sa izmenjenim podacima
+    const storedUser = localStorage.getItem(username);
+    if(storedUser) {
+      try {
+        const u = JSON.parse(storedUser);    
+        if (u.username === username && u.password === password) {  
+
+          console.log("to je taj " + username + " " + password)
+
+          localStorage.setItem("username", u.username);
+          localStorage.setItem("password", u.password);
+
+          localStorage.setItem("name", u.name);
+          localStorage.setItem("surname", u.surname);
+          localStorage.setItem("address", u.address);
+          localStorage.setItem("phone", u.phone);
+
+          router.push("/userHome"); 
+        } else {
+          Swal.fire({
+            text: "Pogrešni podaci.",
+            icon: "warning",
+            iconColor: "#959595",
+            timer: 2000,
+            timerProgressBar: true,
+            showConfirmButton: false
+          });
+          return;
+        }
+      } catch(error) {
+        console.log("Error parsing user data:", error);
+      } 
+      return;
+    }  
+    
+    // ako useru nisu menjani podaci: 
+    const userExists = users.find(user => user.username === username && user.password === password);
+
+    if (userExists) {
+      //localStorage.clear();        
+
+      const userDetails = userData.find(user => user.username === username && user.password === password);
+
+      localStorage.setItem("username", username);
+      localStorage.setItem("password", password);
+
+      localStorage.setItem("name", userDetails.name);
+      localStorage.setItem("surname", userDetails.surname);
+      localStorage.setItem("address", userDetails.address);
+      localStorage.setItem("phone", userDetails.phone);
+
+      router.push("/userHome"); 
+
+    } else {
+
+        Swal.fire({
+          text: "Pogrešni podaci.",
+          icon: "warning",
+          iconColor: "#959595",
+          timer: 2000,
+          timerProgressBar: true,
+          showConfirmButton: false
+        });
+      
+    }
+  }
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className={styles.container}> 
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      <main className={styles.main}>
+
+        <h2>Login</h2>
+
+        <h3>Uloguj se na svoj nalog</h3> 
+
+        <div className={styles.loginForm}>
+          <p className={styles.label}>Korisničko ime:</p>
+          <div className={styles.inputGroup}>
+            
+            <div className={styles.iconWrapper}>
+              <Image src="/user.png" alt="User Icon" width={40} height={40} />
+            </div>
+            <input type="text"  
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  />
+          </div> 
+
+          <p className={styles.label}>Šifra:</p>
+          <div className={styles.inputGroup}>
+            <div className={styles.iconWrapper}>
+              <Image src="/lock.png" alt="Lock Icon" width={52} height={52} />
+            </div>
+            <input type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  />
+          </div>
+
+          <button className="btn" type="submit" onClick={loginUser}>Login</button>
+          <button className="btn" type="submit" onClick={loginUser}>Promena lozinke</button>
+        
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </main> 
+
     </div>
   );
 }
