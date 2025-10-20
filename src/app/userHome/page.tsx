a1c261cba55ba419a8c2ea8b7320b702fe25bf3e
Dusan <dushan.dule@gmail.com>
2025-10-20 22:43:30 +0200

    Added images and text on the user home page. It's finished for now.

--- src/app/userHome/page.tsx
+++ src/app/userHome/page.tsx
@@ -2,7 +2,8 @@
 
 import React, { useState, useEffect } from 'react';
 import styles from "./page.module.css";
-import Menu from '../components/Menu'; 
+import Menu from '../components/Menu';
+import Image from "next/image";
 
 const HomePage = () => {
 
@@ -11,7 +12,20 @@ const HomePage = () => {
             { <Menu/> }
             
             <div className={styles.main}>
-                {/* couple of images, pozarevac, svilajna, glupi tekst */}
+                <Image src="/naslovna1.jpg" alt="Description" width={1440} height={320} className={styles.banner}/>
+
+                <Image src="/pozarevac-danas.jpg" alt="Description" width={1440} height={320} className={styles.poImage}/>
+                <div className={styles.poBlueTint}></div>
+                <p className={styles.poText}>Požarevac</p>
+
+                <Image src="/svilajnac.jpg" alt="Description" width={1440} height={320} className={styles.svilImage}/>
+                <div className={styles.svilBlueTint}></div>
+                <p className={styles.svilText}>Svilajnac</p>
+
+                <div className={styles.welcomeSection}>
+                    <p className={styles.upperText}>Mesto dobre hrane i dobrog drustva</p>
+                    <p className={styles.lowerText}>Dođite da se upoznamo</p>
+                </div>
             </div>
 
         </div>
